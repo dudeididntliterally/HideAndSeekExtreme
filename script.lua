8774b0f3914637c893d3adc033340b7d58726818
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-09-27 19:03:50 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,12 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local MainWindow = OrionLib:MakeWindow({Name = "Hide And Seek Extreme", HidePremium = true, SaveConfig = false, ConfigFolder = "HAS"})
 local Home = MainWindow:MakeTab({
-	Name = "Music",
+	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 local HomeSection = Home:AddSection({
-	Name = "Music"
+	Name = "Main"
 })
 wait()
 OrionLib:MakeNotification({
